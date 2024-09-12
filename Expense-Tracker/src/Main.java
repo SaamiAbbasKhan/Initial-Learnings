80812d6c16b90ab5e4c1cdfbd1092bba267eceef
Saami Abbas Khan <khansaamiabbas@gmail.com>
2024-09-12 23:19:12 +0530

    Update Main.java

--- Expense-Tracker/src/Main.java
+++ Expense-Tracker/src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        // Example code 
         var expenseTracker = new ExpenseTracker();
         expenseTracker.add(new Expenses("Market", 1000));
-        expenseTracker.add(new Expenses("Clothes", 80));
-        expenseTracker.add(new Expenses("Food", 80));
-        expenseTracker.add(new Expenses("Shopping", 80));
-        expenseTracker.add(new Expenses("Dinner", 80));
+        expenseTracker.add(new Expenses("Clothes", 780));
+        expenseTracker.add(new Expenses("Food", 980));
+        expenseTracker.add(new Expenses("Shopping", 700));
+        expenseTracker.add(new Expenses("Dinner", 120));
 
         expenseTracker.categorize();
         expenseTracker.sort();
